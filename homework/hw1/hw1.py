679b4c7d831f849e464c4c332bf6023ab74c47e1
neil399399 <neil399399@gmail.com>
2018-03-23 11:29:52 +0800

    cleanup

--- homework/hw1/hw1.py
+++ homework/hw1/hw1.py
@@ -5,5 +5,5 @@ conf = SparkConf().setMaster("spark://172.17.0.2:7077").setAppName("hw1")
 sc = SparkContext(conf=conf)
 
 # Input data.
-dataset=sc.textFile("file:~homework/Spark-Distributed/homework/hw1/dataset/household_power_consumption.txt")
+dataset=sc.textFile("file:~homework/Spark-Distributed/homework/hw1/household_power_consumption.txt")
 print(dataset.count())
