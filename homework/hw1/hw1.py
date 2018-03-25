a31fe69accd5fe81ce143d167f9af431c0f51dae
neil399399 <neil399399@gmail.com>
2018-03-25 11:09:15 +0800

    test

--- homework/hw1/hw1.py
+++ homework/hw1/hw1.py
@@ -1,3 +1,4 @@
+# init to find pyspark folder.
 import findspark
 findspark.init()
 from pyspark import SparkContext, SparkConf
@@ -7,15 +8,29 @@ sparkMaster="spark://172.17.0.2:7077"
 sparkAppName="hw1"
 sparkExecutorMemory="2g"
 sparkCoreMax="4"
-# init.
+
 # Setting Spark conf.
 conf = SparkConf().setMaster(sparkMaster).setAppName(sparkAppName).set("spark.executor.memory",sparkExecutorMemory)
 sc = SparkContext(conf=conf)
 
 # Input data.
-dataset=sc.textFile("file:/root/homework/dataset/hw1/household_power_consumption.txt")
-print(dataset.count())
+dataset = sc.textFile("file:/root/homework/dataset/hw1/household_power_consumption.txt")
+print("dataset long:",dataset.count())
+header = dataset.first()
+subData1 = dataset.filter(lambda x: x !=header)
+print("subData long:",subData1.count())
 
+# map.
+parserResult = subData1.map(parser)
+print("check parser result:",parserResult,count())
+print("Max global active power:")
+
+
+
+
+def parser(line):
+    values = [float(x) for x in line.split(";")]
+    return values
 
 
 
