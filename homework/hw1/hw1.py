0a27b7591862a4d0cb0ffee20739a0526bd890a5
neil399399 <neil399399@gmail.com>
2018-03-25 14:43:09 +0800

    debug

--- homework/hw1/hw1.py
+++ homework/hw1/hw1.py
@@ -25,12 +25,35 @@ print("dataset long:",dataset.count())
 header = dataset.first()
 subData1 = dataset.filter(lambda x: x !=header)
 print("subData long:",subData1.count())
-# map.
+# map for gap.
 parserResult = subData1.map(parser).filter(lambda x: x[2]!="?")
 gap = parserResult.map(lambda x: float(x[2]))
-print("check parser result:",parserResult.count())
-print("Max global active power:",gap.max())
+# map for grp.
+parserResult2 = subData1.map(parser).filter(lambda x: x[3]!="?")
+grp = parserResult.map(lambda x: float(x[3]))
+# map for voltage.
+parserResult3 = subData1.map(parser).filter(lambda x: x[4]!="?")
+vol = parserResult.map(lambda x: float(x[4]))
+# map for global intensity.
+parserResult4 = subData1.map(parser).filter(lambda x: x[5]!="?")
+gi = parserResult.map(lambda x: float(x[5]))
 
+print("/------------ Question 1 ---------------/")
+print("Max global active power:",gap.max())
+print("Min global active power:",gap.min())
+print("global active power column:",parserResult.count())
+
+print("Max global reactive power:",grp.max())
+print("Min global reactive power:",grp.min())
+print("global reactive power column:",parserResult2.count())
+
+print("Max voltage:",vol.max())
+print("Min voltage",vol.min())
+print("voltage column:",parserResult3.count())
+
+print("Max global intensity:",gi.max())
+print("Min global intensity:",gi.min())
+print("global intensity column:",parserResult4.count())
 
 
 
