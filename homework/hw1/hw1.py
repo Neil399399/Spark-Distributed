e358d2eefa39a09147897649fbe3e10c255e6da3
neil399399 <neil399399@gmail.com>
2018-03-25 14:18:47 +0800

    debug

--- homework/hw1/hw1.py
+++ homework/hw1/hw1.py
@@ -26,7 +26,7 @@ header = dataset.first()
 subData1 = dataset.filter(lambda x: x !=header)
 print("subData long:",subData1.count())
 # map.
-parserResult = subData1.map(parser).map(lambda x: x[3]!="?").map(lambda x: float(x[3])).max()
+parserResult = subData1.map(parser).map(lambda x: x[3]!="?").max()
 print("check parser result:",parserResult)
 print("Max global active power:")
 
