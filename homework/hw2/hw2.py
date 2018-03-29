4f4b23e0543998b3c5fbe0d44effa8c7675e16d3
neil399399 <neil399399@gmail.com>
2018-03-29 17:56:22 +0800

    edit hw2.

--- homework/hw2/hw2.py
+++ homework/hw2/hw2.py
@@ -4,7 +4,7 @@ findspark.init()
 from pyspark import SparkContext, SparkConf
 import csv
 import os
-
+import sys
 dict={}
 
 def Parser(line):
@@ -19,7 +19,7 @@ def TF(line):
             dict[titleWords[x]]=dict[titleWords[x]]+1
         else:
             dict[titleWords[x]]=1
-            
+
     print(dict.items())
     return len(dict)
  
@@ -31,6 +31,8 @@ sparkExecutorMemory="3g"
 sparkDriverMemory="3g"
 sparkCoreMax="4"
 outputFile = "result.txt"
+reload(sys)
+sys.setdefaultencoding('etf8')
 
 # Setting Spark conf.
 conf = SparkConf().setMaster(sparkMaster).setAppName(sparkAppName).set("spark.executor.memory",sparkExecutorMemory).set("spark.driver.memory",sparkDriverMemory)
