944424389c46b2e1821be9119d856bed24ac1b05
neil399399 <neil399399@gmail.com>
2018-03-29 11:33:12 +0800

    edit hw2.

--- homework/hw2/hw2.py
+++ homework/hw2/hw2.py
@@ -40,7 +40,8 @@ print("dataset(RDD) long:",dataset1.count())
 
 # # remove header.
 header = dataset1.first()
-print("header",header.collect())
+headerPrint = header.collect()
+print("header",headerPrint[1])
 subData1 = dataset1.filter(lambda x: x !=header)
 print("subData1:",subData1.count())
 subData2 = subData1.filter(lambda x: x[1])
