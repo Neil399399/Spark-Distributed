21d9bfa715bea6b37715a1dfc984a7fea5074051
neil399399 <neil399399@gmail.com>
2018-03-29 17:45:56 +0800

    edit hw2.

--- homework/hw2/hw2.py
+++ homework/hw2/hw2.py
@@ -19,6 +19,8 @@ def TF(line):
             dict[titleWords[x]]=dict[titleWords[x]]+1
         else:
             dict[titleWords[x]]=1
+            
+    print(dict.items())
     return len(dict)
  
 
