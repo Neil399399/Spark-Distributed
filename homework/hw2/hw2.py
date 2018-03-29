96c1cdec44194f737114082fb84a045e1ce8cd02
neil399399 <neil399399@gmail.com>
2018-03-29 09:55:19 +0800

    add hw2

---
+++ homework/hw2/hw2.py
@@ -0,0 +1,33 @@
+# init to find pyspark folder.
+import findspark
+findspark.init()
+from pyspark import SparkContext, SparkConf
+import csv
+
+
+def Parser(line):
+    values = [x for x in line.split(";")]
+    return values
+
+def Normalization(x):
+    max = x.max()
+    min = x.min()
+    result = x.map(lambda x: (x-min)/(max-min))
+    return result
+
+# Spark configure.
+sparkMaster="spark://172.17.0.5:7077"
+sparkAppName="hw1"
+sparkExecutorMemory="3g"
+sparkDriverMemory="3g"
+sparkCoreMax="4"
+outputFile = "result.txt"
+
+# Setting Spark conf.
+conf = SparkConf().setMaster(sparkMaster).setAppName(sparkAppName).set("spark.executor.memory",sparkExecutorMemory).set("spark.driver.memory",sparkDriverMemory)
+sc = SparkContext(conf=conf)
+
+# Input data.
+dataset = sc.textFile("file:/root/homework/dataset/hw2/News_Final.csv")
+print("dataset long:",dataset.count())
+
