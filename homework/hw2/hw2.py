dd8a9dce8512e3870be506f3b76f061b5a54e4dc
neil399399 <neil399399@gmail.com>
2018-03-31 22:39:22 +0800

    debug hw2.

--- homework/hw2/hw2.py
+++ homework/hw2/hw2.py
@@ -6,18 +6,16 @@ from collections import Counter
 import csv
 import os
 import re
-newDataset=[]
-dict={}
 
 
 def Parser(line):
     values = [x for x in line.split(",")]
     return values
 
-def TF(line):
+# TF
+def TFtitle(line):
     dict={}
     titleWords = [x for x in re.split(' |, |\u2019|\u2018|\u2014|\u2013|\u2026|\u201d|\u201c|:|(|)|\xe1|\xf1|...',line[1].strip())]
-    # headlineWords = [x for x in line[2].split(" ")]
     for x in range(0,len(titleWords)):
         if titleWords[x] in dict:
             dict[titleWords[x]]=dict[titleWords[x]]+1
@@ -25,13 +23,23 @@ def TF(line):
             dict[titleWords[x]]=1
     return dict
 
-def Dict(dictionary):
-    for (key, value) in dictionary.items():
-        if key in dict:
-            dict[key]=dict[key]+1
+def TFheadline(line):
+    dict={}
+    headlineWords = [x for x in re.split(' |, |\u2019|\u2018|\u2014|\u2013|\u2026|\u201d|\u201c|:|(|)|\xe1|\xf1|...',line[2].strip())]
+    for x in range(0,len(headlineWords)):
+        if headlineWords[x] in dict:
+            dict[headlineWords[x]]=dict[headlineWords[x]]+1
         else:
-            dict[key]=1
+            dict[headlineWords[x]]=1
     return dict
+
+def Dict(dictionary,newDictionary):
+    for (key, value) in dictionary.items():
+        if key in newDictionary:
+            newDictionary[key]=newDictionary[key]+1
+        else:
+            newDictionary[key]=1
+    return newDictionary
   
 
  
@@ -63,26 +71,43 @@ header = dataset1.first()
 print("header",header)
 subData1 = dataset1.filter(lambda x: x !=header)
 
-# split.
+# split 4 topic.
 topicObama = subData1.filter(lambda x: x[4]=='obama')
 topicEconomy = subData1.filter(lambda x: x[4]=='economy')
 topicMicrosoft = subData1.filter(lambda x: x[4]=='microsoft')
 topicPalestine = subData1.filter(lambda x: x[4]=='palestine')
 
-#test
-topicObamaDicts = topicObama.map(TF).take(topicObama.count())
-for x in range(0,len(topicObamaDicts)):
-    Dict(topicObamaDicts[x])
+# 4 topic dictionary (title). 
+topicObamaDicts_title = topicObama.map(TFtitle).take(topicObama.count())
+topicEconomyDicts_title = topicEconomy.map(TFtitle).take(topicEconomy.count())
+topicMicrosoftDicts_title = topicMicrosoft.map(TFtitle).take(topicMicrosoft.count())
+topicPalestineDicts_title = topicPalestine.map(TFtitle).take(topicPalestine.count())
 
+# define dictionarys.
+topicObamaDict = {}
+topicEconomyDict = {}
+topicMicrosoftDict = {}
+topicPalestineDict = {}
 
-# # write in file.
-# file = open(outputFile,'a')
-# writer = csv.writer(file)
-# writer.writerow(dict)
-# file.close()
-newDict = Counter(dict)
+# merge all dict.
+for x in range(0,len(topicObamaDicts_title)):
+    Dict(topicObamaDicts_title[x],topicObamaDict)
 
-print(newDict.most_common(10))
+for x in range(0,len(topicEconomyDicts_title)):
+    Dict(topicEconomyDicts_title[x],topicEconomyDict)
+
+for x in range(0,len(topicMicrosoftDicts_title)):
+    Dict(topicMicrosoftDicts_title[x],topicMicrosoftDict)
+
+for x in range(0,len(topicPalestineDicts_title)):
+    Dict(topicPalestineDicts_title[x],topicPalestineDict)
+
+# find top 3 words.
+print(Counter(topicObamaDict).most_common(3))
+print(Counter(topicEconomyDict).most_common(3))
+print(Counter(topicMicrosoftDict).most_common(3))
+print(Counter(topicPalestineDict).most_common(3))
+
 
 
 
