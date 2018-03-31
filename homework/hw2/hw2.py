62ba38d609821455f3558a5ad4a24d5856769a0c
neil399399 <neil399399@gmail.com>
2018-03-31 10:26:37 +0800

    debug hw2.

--- homework/hw2/hw2.py
+++ homework/hw2/hw2.py
@@ -15,7 +15,7 @@ def Parser(line):
 
 def TF(line):
     dict={}
-    titleWords = [x for x in re.split(' |, ',line[1])]
+    titleWords = [x for x in re.split(' |, |\u2019',line[1])]
     # headlineWords = [x for x in line[2].split(" ")]
     for x in range(0,len(titleWords)):
         if titleWords[x] in dict:
