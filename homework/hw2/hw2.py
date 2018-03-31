1d5db808c569f87d78c10c1c939f4dd6dd80ac72
neil399399 <neil399399@gmail.com>
2018-03-31 10:25:25 +0800

    debug hw2.

--- homework/hw2/hw2.py
+++ homework/hw2/hw2.py
@@ -70,9 +70,7 @@ topicPalestine = subData1.filter(lambda x: x[4]=='palestine')
 
 #test
 test = topicObama.map(TF)
-test2 = test.take(2)
-print(test2[0])
-print(test2[1])
+test2 = test.take(100)
 for x in range(0,len(test2)):
     Dict(test2[x])
 print(dict)
