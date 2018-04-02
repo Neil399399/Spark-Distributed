1b2ad4b429c5a6e0ff7a556b78723f0d8c1a1ffe
neil399399 <neil399399@gmail.com>
2018-04-02 09:05:17 +0800

    debug hw2

--- homework/hw2/hw2.py
+++ homework/hw2/hw2.py
@@ -108,7 +108,7 @@ for x in range(0,len(dates_dict)):
 
 for (key, value) in dict.items():
     for ID in value:
-        print(key,value)
+        print(key,ID)
         temp = subData1.filter(lambda x: x[0]==ID)
         topicObamaDicts_title = temp.map(TFtitle).collect()
 
